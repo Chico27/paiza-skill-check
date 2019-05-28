5e1ce30cff49af34d707fc83ba87b983d54acc6f
Chico27 <masamasa.987654321@gmail.com>
2019-05-28 18:50:42 +0900

    new

---
+++ D029.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+input_line = input()
+print(7-int(input_line))
---
+++ D056.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+
+input_line = input()
+
+r1,r2=input_line.split(" ")
+
+print(int(r1)**3 - int(r2)**3)
