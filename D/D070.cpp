d9caca750aaffabd0bbaea688b4f01e406069067
Chico27 <masamasa.987654321@gmail.com>
2019-06-02 20:06:41 +0900

    new

---
+++ D/D070.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+  int n = 2;
+  int token1, token2;
+  for(int i = 0; i < n; i++) {
+    cin >> token1 >> token2;
+  }
+
+    cout << token1 - token2 << endl;
+    return 0;
+}
